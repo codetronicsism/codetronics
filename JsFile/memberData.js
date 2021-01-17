36e865bb7893f7990fc0a43b0af1e617c144f7ce
codetronicsism <77583503+codetronicsism@users.noreply.github.com>
2021-01-17 22:50:32 +0530

    Add files via upload

---
+++ JsFile/memberData.js
@@ -0,0 +1,154 @@
+// Add member at the end
+
+const memberData = [
+    {
+        memberName: "Anshuman Choudhary",
+        memberDescription:  "Hi, I am Anshuman Choudhary, I am a Competitive Programmer and ML Enthusiast. || ECE 2021",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com/profile/AC_007",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Yash Ranka",
+        memberDescription:"Hi, I am Yash Ranka, I am a Competitive Programmer and Full stack Web Developer. || ECE 2021",
+        memberImage: "./images/yash_ranka.jpeg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Ankur Chourasia",
+        memberDescription:"Hi I am Ankur Chourasia, I am a Competitive Programmer and ML Enthusiast. || ECE 2021",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Nitin Modi",
+        memberDescription:"Hi I am Nitin Modi, I am a Competitive Programmer and ML Enthusiast. || ECE 2021",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Nikhil Arora",
+        memberDescription:"Hi I am Nikhil Arora, I am a Competitive Programmer and ML Enthusiast. || ECE 2021",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Pradyumn Neema",
+        memberDescription:"Hi I am Pradyumn Neema, I am a Competitive Programmer and ML Enthusiast. || ECE 2021",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Ujjwal Keshri",
+        memberDescription:"Hi, I am Ujjwal Keshri, I am a Competitive Programmer and Web Developer. || ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com/profile/ujjwal_keshri",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Yash Sahu",
+        memberDescription:"Hi I am Yash Sahu, I am a Competitive Programmer, Web Developer and ML Enthusiast. || ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Umang Somtiya",
+        memberDescription:"Hi I am Umang Somtiya, I am a Competitive Programmer and Web Developer. || ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Amar Lal",
+        memberDescription:"Hi I am Amar Lal, I am a Competitive Programmer and ML Enthusiast.|| EE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Naman Joshi",
+        memberDescription:"Hi I am Naman Joshi, I am a Competitive Programmer and ML Enthusiast.|| EE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Froz Hussain",
+        memberDescription:"Hi I am Froz Hussain, I am a Competitive Programmer and ML Enthusiast.|| ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Shruti Simran",
+        memberDescription:"Hi I am Shruti Simran, I am a Competitive Programmer and ML Enthusiast.|| ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Udit Khemka",
+        memberDescription:"Hi I am Udit Khemka, I am a Competitive Programmer and Web Developer.|| ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    },
+    {
+        memberName: "Utkarsh Kumar",
+        memberDescription:"Hi I am Utkarsh Kumar, I am a Competitive Programmer and ML Enthusiast.|| ECE 2022",
+        memberImage: "./images/banner4.jpg",
+        codeforcesLink: "https://codeforces.com",
+        codechefLink: "https://codechef.com",
+        leetcode: "https://leetcode.com",
+        hackerrank: "https://hackerrank.com",
+        linkedIn: "https://linkedin.com"
+    }
+];
