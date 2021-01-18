e61117c3270a5f0b170908c2b73edbb3028b5dec
codetronicsism <77583503+codetronicsism@users.noreply.github.com>
2021-01-18 14:55:00 +0530

    Update resourceData.js

--- JsFile/resourceData.js
+++ JsFile/resourceData.js
@@ -3,133 +3,151 @@ const topic1 = [
     itemText1: "Tutorial",
     itemText2: "Containers",
     link1: "https://www.topcoder.com/community/competitive-programming/tutorials/power-up-c-with-the-standard-template-library-part-1/",
-    link2: "https://www.topcoder.com/community/competitive-programming/tutorials/power-up-c-with-the-standard-template-library-part-2/"
+    linkText: "Topcoder"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Containers",
+    link1: "https://www.topcoder.com/community/competitive-programming/tutorials/power-up-c-with-the-standard-template-library-part-2/",
+    linkText: "Topcoder"
   },
   {
     itemText1: "Tutorial",
     itemText2: "STL",
     link1: "https://www.geeksforgeeks.org/difference-between-iterators-and-pointers-in-c-c-with-examples/?ref=rp",
-    link2: "https://www.hackerearth.com/practice/notes/standard-template-library/"
+    linkText: "GFG"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "STL",
+    link1: "https://www.hackerearth.com/practice/notes/standard-template-library/",
+    linkText: "GFG"
   },
   {
     itemText1: "Tutorial",
     itemText2: "STL",
     link1: "https://www.cplusplus.com/reference/stl/",
-    link2: "https://www.geeksforgeeks.org/the-c-standard-template-library-stl/"
+    linkText: "CPlusPlus"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "STL",
+    link1: "https://www.geeksforgeeks.org/the-c-standard-template-library-stl/",
+    linkText: "GFG"
   },
   {
     itemText1: "Tutorial",
     itemText2: "Doc used in class",
     link1: "https://docs.google.com/document/d/1T-IAzwGObHsPuA_kQbg-9JnSikDEAt9t2tmDUlhhU8Q/edit",
-    link2: "#"
+    linkText: "DOC"
   },
   {
     itemText1: "Problem",
     itemText2: "Q1",
     link1: "https://www.hackerrank.com/challenges/jesse-and-cookies/problem",
-    link2: "#"
+    linkText: "Hackerearth"
   },
   {
     itemText1: "Problem",
     itemText2: "Q2",
     link1: "https://www.hackerrank.com/challenges/qheap1/problem",
-    link2: "#"
+    linkText: "Hackerearth"
   },
   {
     itemText1: "Problem",
     itemText2: "Q3",
     link1: "https://www.spoj.com/problems/ACMCEG2B/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q4",
     link1: "https://www.spoj.com/problems/ACMCEG2C/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q5",
     link1: "https://www.spoj.com/problems/DOTAA2/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q6",
     link1: "https://www.spoj.com/problems/MINSTOCK/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q7",
     link1: "https://www.spoj.com/problems/RPLD/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q8",
     link1: "https://codeforces.com/contest/903/problem/C",
-    link2: "#"
+    linkText: "Codeforces"
   },
   {
     itemText1: "Problem",
     itemText2: "Q9",
     link1: "https://atcoder.jp/contests/arc087/tasks/arc087_a",
-    link2: "#"
+    linkText: "Atcoder"
   },
   {
     itemText1: "Problem",
     itemText2: "Q10",
     link1: "https://codeforces.com/contest/855/problem/A",
-    link2: "#"
+    linkText: "Codeforces"
   },
   {
     itemText1: "Problem",
     itemText2: "Q11",
     link1: "https://practice.geeksforgeeks.org/problems/unique-rows-in-boolean-matrix/1",
-    link2: "#"
+    linkText: "GFG"
   },
   {
     itemText1: "Problem",
     itemText2: "Q12",
     link1: "https://www.geeksforgeeks.org/maximum-product-increasing-subsequence-size-3/",
-    link2: "#"
+    linkText: "GFG"
   },
   {
     itemText1: "Problem",
     itemText2: "Q13",
     link1: "https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/seating-arrangement-6b8562ad/",
-    link2: "#"
+    linkText: "Hackerearth"
   },
   {
     itemText1: "Problem",
     itemText2: "Q14",
     link1: "https://www.hackerearth.com/practice/data-structures/queues/basics-of-queues/practice-problems/algorithm/number-recovery-0b988eb2/description/",
-    link2: "#"
+    linkText: "Hackerearth"
   },
   {
     itemText1: "Problem",
     itemText2: "Q15",
     link1: "https://www.spoj.com/problems/MINSTACK/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q16",
     link1: "https://www.spoj.com/problems/STPAR/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q17",
     link1: "https://www.spoj.com/problems/ONP/",
-    link2: "#"
+    linkText: "SPOJ"
   },
   {
     itemText1: "Problem",
     itemText2: "Q18",
     link1: "https://www.codechef.com/problems/WASHHAND",
-    link2: "#"
+    linkText: "Codechef"
   }
 ];
 const topic2 = [
