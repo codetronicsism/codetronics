8ebf2f386ef0c93ec5c4fb4bd776268e86ca9966
codetronicsism <77583503+codetronicsism@users.noreply.github.com>
2021-01-24 20:35:19 +0530

    Update resourceData.js

--- JsFile/resourceData.js
+++ JsFile/resourceData.js
@@ -151,10 +151,176 @@ const topic1 = [
   }
 ];
 const topic2 = [
-  
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number Theory-I",
+    link1: "https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-2/tutorial/",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number Theory-I",
+    link1: "https://cp-algorithms.com/algebra/factorization.html",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number Theory-I",
+    link1: "https://cp-algorithms.com/algebra/divisors.html",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number Theory-I",
+    link1: "https://drive.google.com/file/d/1kB9fUGaqpT4YVkCK0gjg35jVOFPqfWnp/view?usp=sharing",
+    linkText: "Doc"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q1",
+    link1: "https://codeforces.com/contest/230/problem/B",
+    linkText: "Codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q2",
+    link1: "https://codeforces.com/contest/1165/problem/D",
+    linkText: "Codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q3",
+    link1: "https://codeforces.com/problemset/problem/26/A",
+    linkText: "Codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q4",
+    link1: "https://codeforces.com/problemset/problem/1294/C",
+    linkText: "codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q5",
+    link1: "https://www.spoj.com/problems/TDPRIMES/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q6",
+    link1: "https://www.spoj.com/problems/NGIRL/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q7",
+    link1: "https://www.spoj.com/problems/PRIME1/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q8",
+    link1: "https://www.spoj.com/problems/PTRI2/",
+    linkText: "spoj"
+  }
 ];
 const topic3 = [
-  
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number theory-II",
+    link1: "https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-1/tutorial/",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number theory-II",
+    link1: "https://cp-algorithms.com/algebra/binary-exp.html",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number theory-II",
+    link1: "https://cp-algorithms.com/algebra/phi-function.html",
+    linkText: "Blog"
+  },
+  {
+    itemText1: "Tutorial",
+    itemText2: "Number theory-II",
+    link1: "  https://drive.google.com/file/d/1ehc30oqvwhpr7ONVyT6kuJaEp7-NgbSv/view?usp=sharing",
+    linkText: "doc"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q1",
+    link1: "https://codeforces.com/problemset/problem/1285/C",
+    linkText: "Codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q2",
+    link1: "https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-1/practice-problems/algorithm/the-confused-monk/description/",
+    linkText: "hackerearth"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q3",
+    link1: "https://www.interviewbit.com/problems/largest-coprime-divisor/",
+    linkText: "interviebit"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q4",
+    link1: "https://codeforces.com/problemset/problem/913/A",
+    linkText: "codeforces"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q5",
+    link1: "https://www.codechef.com/problems/FEXP",
+    linkText: "codechef"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q6",
+    link1: "https://www.spoj.com/problems/LASTDIG/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q7",
+    link1: "https://www.spoj.com/problems/ZSUM/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q8",
+    link1: "https://www.interviewbit.com/problems/rearrange-array/",
+    linkText: "interviebit"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q9",
+    link1: "https://www.hackerrank.com/challenges/sherlock-and-gcd/problem",
+    linkText: "hackerrank"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q10",
+    link1: "https://www.hackerearth.com/practice/math/number-theory/totient-function/practice-problems/algorithm/exam-time-2/",
+    linkText: "hackerearth"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q11",
+    link1: "https://www.spoj.com/problems/LCMSUM/",
+    linkText: "spoj"
+  },
+  {
+    itemText1: "Problem",
+    itemText2: "Q12",
+    link1: "https://codeforces.com/problemset/problem/300/C",
+    linkText: "codeforces"
+  }
 ];
 const topic4 = [
   
@@ -175,20 +341,5 @@ const topic9 = [
   
 ];
 const topic10 = [
-  
-];
-const topic11 = [
-  
-];
-const topic12 = [
-  
-];
-const topic13 = [
-  
-];
-const topic14 = [
-  
-];
-const topic15 = [
   
 ];
